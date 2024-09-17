2ec73d64a9a9158616d7196e14c236ca6a5b908a
hargussss <ivkarpich@edu.hse.ru>
2024-09-17 22:07:09 +0300

    3rd try to fix errors in commit history

--- lib_pair/pair.cpp
+++ lib_pair/pair.cpp
@@ -1,1 +1,3 @@
 // Copyright 2024 Ivan Karpich
+
+#include "../lib_pair/pair.h"
--- lib_pair/pair.h
+++ lib_pair/pair.h
@@ -1,1 +1,6 @@
 // Copyright 2024 Ivan Karpich
+
+#ifndef LIB_PAIR_PAIR_H_
+#define LIB_PAIR_PAIR_H_
+
+#endif  // LIB_PAIR_PAIR_H_
